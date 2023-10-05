9551c3e28f433a19787579d62d6ac0cb0cfe6ca5
JollyF <angelo.russi96@gmail.com>
2023-10-05 22:37:24 +0200

    Changed site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Casper',
-    description: 'A port of the casper blog built for gatsby',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'Dr. Spinelli Gianvito',
+    description: 'Il vostro odontoriatra di fiducia',
+    siteUrl: 'https://dr.spinelli.netlify.com', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml.name',
